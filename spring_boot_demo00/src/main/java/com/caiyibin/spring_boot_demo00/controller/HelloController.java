1dcb5e6401a65d613f7b5334b5a8795cedf734d7
caiyibin <748282966@qq.com>
2018-08-01 19:46:30 +0800

    20180801

--- spring_boot_demo00/src/main/java/com/caiyibin/spring_boot_demo00/controller/HelloController.java
+++ spring_boot_demo00/src/main/java/com/caiyibin/spring_boot_demo00/controller/HelloController.java
@@ -1,14 +1,31 @@
 package com.caiyibin.spring_boot_demo00.controller;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @RestController
 @EnableAutoConfiguration
 public class HelloController {
     @RequestMapping("/hello")
-    private String index(){
+    private String index() {
         return "Hello World!";
     }
+/*
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        // 目的是
+        return args -> {
+            System.out.println("来看看 SpringBoot 默认为我们提供的 Bean：");
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            Arrays.stream(beanNames).forEach(System.out::println);
+        };
+    }
+*/
 }
